52ed8b120866b52deab5fb23267d97bdaea57137
OsandaGagana <Osandagds@gmail.com>
2025-06-15 11:35:30 +0530

    fourth commit

---
+++ src/edu/ijse/layered/dao/custom/CustomerDao.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+package edu.ijse.layered.dao.custom;
+
+import edu.ijse.layered.dao.CrudDao;
+import edu.ijse.layered.entity.CustomerEntity;
+
+/**
+ *
+ * @author Anjana
+ */
+public interface CustomerDao extends CrudDao<CustomerEntity, String>{
+    
+}
